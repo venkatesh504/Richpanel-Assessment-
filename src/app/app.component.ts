7bcb9d7774998ca709bf4886279bdcf7e5551b0f
Solomon <solomonnallapu@gmail.com>
2019-04-14 01:54:56 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {list}  from './../assets/json/list.json';
-import {MatBottomSheet, MatBottomSheetRef} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +9,29 @@ export class AppComponent {
   title = 'Webapp';
   taskList : any = list;
   task_name:any;
-  due_indexs : any;
-  // constructor() {}
-
-  d: Date = new Date();  
-  // console.log(taskList);
-  add_task(){
-    console.log( this.task_name);
+  page_status:boolean = true;
+  update_taskName:any;
+  add_taskname:any;
+  onUpdate(list){
+    
+    this.page_status =!true;
+    this.update_taskName = list;
+    this.task_name = this.update_taskName;
+    
+  }
+  onClose(){
+    this.page_status = true ;
+  }
+  gopage(){
+    this.page_status = !true;
+  }
+  add_task( ){
+  
    this.taskList.push(this.task_name);
+   console.log(this.add_task);
+   
+   this.page_status = true;
+   
   
   }
 }
