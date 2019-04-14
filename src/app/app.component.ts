222593e6ae5052a0104c2bb7c9ecf128065b6e1a
YAMJAL VENKATESH <yamjal.venkatesh@gmail.com>
2019-04-14 17:04:29 +0530

    delet option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,26 +14,36 @@ export class AppComponent {
   update_taskName:any;
   add_taskname:any;
   due_tasks:any = 0;
-
   jsonlist:any;
+  index_value: any;
+  dragStatus:boolean = true;
+
+  constructor(){
+    this.due_tasks = this.taskList.length;
+    console.log("function comming");
+    
+  }
   onSelect(list , i){
     var list_1 :any;
+    console.log(this.index_value);
+    
     this.page_status =!true;
     this.update_taskName = list;
     this.task_name = this.update_taskName;
     for (var _i = 0, _a = this.taskList; _i < _a.length; _i++) {
-       list_1 = _i;      
+       list_1 = _i;   
+       if(this.index_value == list_1){
+        this.update_check= false;
+        console.log("updated");
+         }
+      else{
+       
+        console.log("new");
+        
+      }   
       
     }
-    if(i === list_1){
-      this.update_check= false;
-      console.log("updated");
-       }
-    else{
-     
-      console.log("new");
-      
-    }
+    
   }
   onClose(){
     this.page_status = true ;
@@ -43,16 +53,70 @@ export class AppComponent {
     this.task_name ="";
   }
   add_task(){
+    var list_1 :any;
+    for (var _i = 0, _a = this.taskList; _i < _a.length; _i++) {
+      list_1 = _i;      
+     
+   }
     this.page_status = true;
     if(this.update_check === true){
+      this.taskList.push(this.task_name);
       console.log("check fail update fail");
       
     }
     else{
       console.log("check success updar");
       this.update_check = true 
+      this.taskList.push(this.index_value);
     }
-   this.taskList.push(this.task_name);
+
    this.due_tasks = this.taskList.length;
-  }
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+allowDrop(ev)
+{
+ev.preventDefault();
+console.log("all drop");
+
+}
+drag(ev , i )
+{
+ev.dataTransfer.setData("Text",ev.target.id);
+console.log("drag is setting data");
+console.log(i);
+
+this.dragStatus = false;
+return this.index_value = i
+
+}
+ drop(ev )
+{
+console.log(this.index_value);
+
+  console.log("dra is roping");
+  
+ev.stopPropagation();
+var data=ev.dataTransfer.getData("Text");
+var el = document.getElementById(data);
+// el.parentNode.removeChild(el);
+this.taskList.pop(this.index_value);
+this.dragStatus= true;
+console.log("drag status true");
+this.due_tasks = this.taskList.length;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatDialogModule} from '@angular/material/dialog';
+
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -16,8 +14,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule,
-    MatButtonModule, MatCheckboxModule,
+
+
     FormsModule
 
   ],
