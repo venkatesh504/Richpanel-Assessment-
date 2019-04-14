4f647c9e0371ce63627bdeee607bef936f68c09b
YAMJAL VENKATESH <yamjal.venkatesh@gmail.com>
2019-04-15 00:49:02 +0530

    fkjgfdgkj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   jsonlist:any;
   index_value: any;
   dragStatus:boolean = true;
-
+addclick:boolean=false;
   constructor(){
     this.due_tasks = this.taskList.length;
     console.log("function comming");
@@ -31,28 +31,24 @@ export class AppComponent {
     this.update_taskName = list;
     this.task_name = this.update_taskName;
     for (var _i = 0, _a = this.taskList; _i < _a.length; _i++) {
-       list_1 = _i;   
-       if(this.index_value == list_1){
-        this.update_check= false;
-        console.log("updated");
-         }
-      else{
-       
-        console.log("new");
-        
-      }   
+       list_1 = _i;      
       
     }
-    
+
   }
+
   onClose(){
     this.page_status = true ;
   }
   gopage(){
+    this.addclick = !this.addclick; 
     this.page_status = !true;
     this.task_name ="";
+    
   }
-  add_task(){
+  
+  add_task( list){
+  
     var list_1 :any;
     for (var _i = 0, _a = this.taskList; _i < _a.length; _i++) {
       list_1 = _i;      
@@ -67,7 +63,7 @@ export class AppComponent {
     else{
       console.log("check success updar");
       this.update_check = true 
-      this.taskList.push(this.index_value);
+      this.taskList.splice(index,this.index_value , this.task_name);
     }
 
    this.due_tasks = this.taskList.length;
