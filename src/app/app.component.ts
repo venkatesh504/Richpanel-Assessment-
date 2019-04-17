4e4f0e0dabc5a6897b8d4fd2c13e0f43f771ecb7
Venkatesh Y <venkatesh.y@way2online.co.in>
2019-04-17 18:50:25 +0530

    animations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,11 @@ export class AppComponent {
   pageStatus: boolean = true;
   droppedData: string;
   addclick: boolean = false;
-  plus:boolean = true;
-  pluserotate:boolean=false;
-
+  plus: any ;
+  pluserotate: boolean = false;
+  circle_status: any = "none";
+  index_value: any;
+  delete_class:any;
   // this function is for updating the stored records
   onSelect(list, i) {
     this.newTask = false;
@@ -30,54 +32,56 @@ export class AppComponent {
   // closing the task popup
   onClose() {
     this.pageStatus = true;
-    this.plus = !this.plus;
-    return this.plus 
+  
+    this.circle_status = "close";
+    console.log("in close",this.circle_status);
+    
+    console.log("close status",this.circle_status);
+    
   }
+
   // Rediction of add for add button.
   gopage() {
     this.addclick = !this.addclick;
     this.pageStatus = !this.pageStatus;
+    this.circle_status = "open";
+    console.log("in go page open",this.circle_status);
     this.taskName = '';
   }
   // Adding the new task and updating the task
   add_task(taskName) {
-    if ((this.constIndex || this.constIndex === 0) && !this.newTask) {
+    if ((this.constIndex || this.constIndex === 0) && !this.newTask) 
+    {
       this.totalTasks[this.constIndex] = taskName;
       this.newTask = true;
-    } else if (this.newTask) {
+    } 
+    else if (this.newTask) 
+    {
       this.totalTasks.push(taskName);
     }
     this.pageStatus = true;
+    this.circle_status = "close";
   }
   // After leaves the drag.
   dragEnd(event, index) {
     this.deleteEnable = false;
-    console.log("draing");
-    
+    this.index_value = index;
+    console.log("end working");
+    return this.index_value;
   }
   // Enable the delete button after drag
   dragStart() {
     this.deleteEnable = true;
-    console.log("drag started");
-    
-    
   }
   // Deleting the element from array after drop.
   onDrop({ dropData }: DropEvent<string>): void {
     this.droppedData = dropData;
-    this.totalTasks.splice(dropData, 1);
+    this.totalTasks.splice(this.index_value, 1);
     setTimeout(() => {
       this.droppedData = '';
-    }, 2000);
-this.plus= !this.plus;
-console.log("drag ended");
 
+    }, 2000);
+    this.plus = 'rotate';
   }
 
-// this.myModel = 'An editable text';
-// myValidator = function(newValue)
-// {
-//   // a simple required field:
-//   return !!newValue;
-// }
 }
